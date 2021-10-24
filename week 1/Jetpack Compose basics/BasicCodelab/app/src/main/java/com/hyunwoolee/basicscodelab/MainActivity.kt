3d63f121efbaab63141e1988dffbbf0f25a5e549
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-10-24 15:49:21 +0900

    State hoisting: share state to multiple function

--- week 1/Jetpack Compose basics/BasicCodelab/app/src/main/java/com/hyunwoolee/basicscodelab/MainActivity.kt
+++ week 1/Jetpack Compose basics/BasicCodelab/app/src/main/java/com/hyunwoolee/basicscodelab/MainActivity.kt
@@ -3,21 +3,14 @@ package com.hyunwoolee.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.OutlinedButton
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.hyunwoolee.basicscodelab.ui.theme.BasicsCodelabTheme
-import kotlin.math.exp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -33,9 +26,9 @@ class MainActivity : ComponentActivity() {
 @Composable
 private fun Greeting(name: String) {
     // Recomposition이 일어날 때, remember가 붙은 변수는 Recomposition 대상에서 제외된다
-    val expanded = remember { mutableStateOf(false) }
+    var expanded by remember { mutableStateOf(false) }
 
-    val extraPagging = if (expanded.value) 48.dp else 0.dp
+    val extraPadding = if (expanded) 48.dp else 0.dp
     // Surface를 MaterialTheme으로 지정했는데 글자 색이 하얀색으로 바뀜
     /*
     * In this case, Surface understands that, when the background is set to the primary color,
@@ -49,24 +42,56 @@ private fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier.weight(1f)
-                    .padding(bottom = extraPagging)
+                    .padding(bottom = extraPadding)
             ) {
                 Text(text = "Hello")
                 Text(text = name)
             }
             OutlinedButton(
-                onClick = { expanded.value = !expanded.value }
+                onClick = { expanded = !expanded }
             ) {
-                Text(if (expanded.value) "Show less" else "Show more")
+                Text(if (expanded) "Show less" else "Show more")
             }
         }
     }
 }
 
 @Composable
-private fun MyApp(names: List<String> = listOf("World", "Compose")) {
+private fun Greetings(names: List<String> = listOf("World", "Compose")) {
     Column(modifier = Modifier.padding(vertical = 4.dp)) {
         names.forEach { Greeting(it) }
+    }
+}
+
+@Composable
+private fun OnboardingScreen(onContinueClicked: () -> Unit) {
+
+
+    Surface {
+        Column(
+            modifier = Modifier.fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text("Welcome to the Basics Codelab!")
+            Button(
+                modifier = Modifier.padding(vertical = 24.dp),
+                onClick = onContinueClicked
+            ) {
+                Text("Continue")
+            }
+        }
+    }
+}
+
+@Composable
+private fun MyApp() {
+    var shouldShowOnboarding by remember { mutableStateOf(true) }
+
+    if (shouldShowOnboarding) {
+        OnboardingScreen { shouldShowOnboarding = false }
+    } else {
+        Greetings()
     }
 }
 
