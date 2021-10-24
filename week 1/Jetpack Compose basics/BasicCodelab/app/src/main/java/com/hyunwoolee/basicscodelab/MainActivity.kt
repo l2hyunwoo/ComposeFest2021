526ac678dedaf628d02102cc9388dadffa2c37c3
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-10-24 16:38:23 +0900

    Animation with animateDpAsState + Spring Animation

--- week 1/Jetpack Compose basics/BasicCodelab/app/src/main/java/com/hyunwoolee/basicscodelab/MainActivity.kt
+++ week 1/Jetpack Compose basics/BasicCodelab/app/src/main/java/com/hyunwoolee/basicscodelab/MainActivity.kt
@@ -3,11 +3,15 @@ package com.hyunwoolee.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.core.Spring
+import androidx.compose.animation.core.animateDpAsState
+import androidx.compose.animation.core.spring
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -30,7 +34,14 @@ private fun Greeting(name: String) {
     // Recomposition이 일어날 때, remember가 붙은 변수는 Recomposition 대상에서 제외된다
     var expanded by remember { mutableStateOf(false) }
 
-    val extraPadding = if (expanded) 48.dp else 0.dp
+    val extraPadding by animateDpAsState(
+        targetValue = if (expanded) 48.dp else 0.dp,
+        animationSpec = spring(
+            dampingRatio = Spring.DampingRatioHighBouncy,
+            stiffness = Spring.StiffnessLow
+        )
+    )
+
     // Surface를 MaterialTheme으로 지정했는데 글자 색이 하얀색으로 바뀜
     /*
     * In this case, Surface understands that, when the background is set to the primary color,
@@ -44,7 +55,7 @@ private fun Greeting(name: String) {
         Row(modifier = Modifier.padding(24.dp)) {
             Column(
                 modifier = Modifier.weight(1f)
-                    .padding(bottom = extraPadding)
+                    .padding(bottom = extraPadding.coerceAtLeast(0.dp))
             ) {
                 Text(text = "Hello")
                 Text(text = name)
@@ -63,6 +74,11 @@ private fun Greetings(names: List<String> = List(1000) { "$it" }) {
 //    Column(modifier = Modifier.padding(vertical = 4.dp)) {
 //        names.forEach { Greeting(it) }
 //    }
+    /*
+    * LazyColumn doesn't recycle its children like RecyclerView.
+    * It emits new Composables as you scroll through it and is still performant,
+    * as emitting Composables is relatively cheap compared to instantiating Android Views.
+    * */
     LazyColumn(modifier = Modifier.padding(vertical = 4.dp)) {
         items(names) { name -> Greeting(name) }
     }
@@ -89,7 +105,10 @@ private fun OnboardingScreen(onContinueClicked: () -> Unit) {
 
 @Composable
 private fun MyApp() {
-    var shouldShowOnboarding by remember { mutableStateOf(true) }
+    /*
+    * Configuration Change가 일어나도 State가 초기화되지 않게(ViewModel property와 비슷하넹)
+    * */
+    var shouldShowOnboarding by rememberSaveable { mutableStateOf(true) }
 
     if (shouldShowOnboarding) {
         OnboardingScreen { shouldShowOnboarding = false }
