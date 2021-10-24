865283db5d27dee10dc370fe5d188ad40b45728c
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-10-24 14:46:46 +0900

    Theming(with Material Design Component) and Modifier

--- week 1/Jetpack Compose basics/BasicCodelab/app/src/main/java/com/hyunwoolee/basicscodelab/MainActivity.kt
+++ week 1/Jetpack Compose basics/BasicCodelab/app/src/main/java/com/hyunwoolee/basicscodelab/MainActivity.kt
@@ -3,11 +3,14 @@ package com.hyunwoolee.basicscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hyunwoolee.basicscodelab.ui.theme.BasicsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -26,7 +29,15 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 private fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+    // Surface를 MaterialTheme으로 지정했는데 글자 색이 하얀색으로 바뀜
+    /*
+    * In this case, Surface understands that, when the background is set to the primary color,
+    * any text on top of it should use the onPrimary color, which is also defined in the theme.
+    * You can learn more about this in the Theming your app section.
+    * */
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!", modifier = Modifier.padding(24.dp))
+    }
 }
 
 @Preview(showBackground = true)
