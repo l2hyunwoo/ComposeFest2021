9ce6443e57de537d5cab4ade61219aec96b3fdc0
Hyun Woo Lee <l2hyunwoo@gmail.com>
2021-11-20 16:52:48 +0900

    [Week 2] Layout Codelab - Modifiers

--- week 2/Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/hyunwoolee/layoutscodelab/MainActivity.kt
+++ week 2/Layouts in JetpackCompose/LayoutsCodelab/app/src/main/java/com/hyunwoolee/layoutscodelab/MainActivity.kt
@@ -3,11 +3,23 @@ package com.hyunwoolee.layoutscodelab
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.hyunwoolee.layoutscodelab.ui.theme.LayoutsCodelabTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,6 +34,48 @@ class MainActivity : ComponentActivity() {
             }
         }
     }
+}
+
+/*
+* Most composables accept an optional modifier parameter to make them more flexible
+* If you're creating your own composable, consider having a modifier as a parameter,
+* default it to Modifier
+*
+* By convention, the modifier is specified as the first optional parameter of a function.
+* This enables you to specify a modifier on a composable without having to name all parameters.
+* */
+@Composable
+fun PhotographerCard(modifier: Modifier = Modifier) {
+    Row(
+        modifier = modifier
+            .padding(8.dp)
+            .clip(RoundedCornerShape(4.dp))
+            .background(MaterialTheme.colors.surface)
+            .clickable { }
+            .padding(16.dp)
+    ) {
+        Surface(
+            modifier = Modifier.size(50.dp),
+            shape = CircleShape,
+            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+        ) {
+
+        }
+        Column(
+            modifier = Modifier.padding(start = 8.dp)
+        ) {
+            Text(text = "HyunWoo Lee", fontWeight = FontWeight.Bold)
+            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
+                Text(text = "Nunu Lee", style = MaterialTheme.typography.body2)
+            }
+        }
+    }
+}
+
+@Preview(name = "PhotographerCardPreview")
+@Composable
+fun PhotographerCardPreview() {
+    PhotographerCard()
 }
 
 @Composable
